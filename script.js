815e8e96a609762b42a5d644855b0bc97bd37e35
lil_kent1 <aubryquentin700@gmail.com>
2020-03-20 16:58:10 +0100

    update mini-tp 2

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const fs = require('fs');
+const http = require('http');
+const pug = require('pug');
 
-const filename = process.argv[2];
+
+const port = process.argv[2]
+const filename = process.argv[3];
+
+
+if (!port) {
+    console.error('Please provide a port number')
+    process.exit(1);
+}
 
 if (!filename) {
     console.error('Please provide a filename');
     process.exit(1);
 }
 
-fs.readFile(filename,'utf8', (err,data) => {
-    if (err) {
-        console.log(err);
-        return ;
+const compiledFunction = pug.compileFile('template.pug');
+
+
+
+const server = http.createServer(
+    (req, res) => {
+
+        fs.readFile(filename,'utf8', (err,data) => {
+            if (err) {
+                console.log(err);
+                return ;
+            }
+            const generatedTemplate = compiledFunction({
+                name: 'ca',
+                utilisateurs: JSON.parse(data)
+            })
+    
+            
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(generatedTemplate)
+        });
     }
-    console.log(data);
+);
+
+server.listen(port, () => {
+    console.log(`➡️  your server is running on port ${port}`)
 })
+
+
